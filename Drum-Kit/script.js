c376746fc7c99963f7a81892a88e8cd3b50ed858
Smriti Bhati <smriti.bhati@gmail.com>
2020-03-23 22:40:23 +0530

    :musical_note:

--- Drum-Kit/script.js
+++ Drum-Kit/script.js
@@ -1,9 +1,23 @@
-function myFunction(id,url){
-    // document.getElementById(id).className = "played";
-    // myVar = setTimeout(removeclass, 2000);
-    // document.getElementById(id).className = "btnbox";
-    new Audio(url).play();
+function rmvtransition(e){
+    // console.log(e);
+    // if(e.propertyName!='transform')
+    //     return;
+    e.target.classList.remove('playing');
 }
-function removeclass(){
+function playsound(e){
+    const btn = document.querySelector(`.btnbox[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   
+    if(audio==null)
+        return;
+    audio.currentTime=0;
+    audio.play();
+    btn.classList.add("playing");
+}
 
-}
+const btns = document.querySelectorAll('.btnbox');
+btns.forEach(element => {
+    element.addEventListener('transitionend',rmvtransition);
+});
+
+window.addEventListener('keydown',playsound);
