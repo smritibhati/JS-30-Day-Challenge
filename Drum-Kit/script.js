3416eb68255a8d2b6b51b946477fe57150c5cbda
Smriti Bhati <smriti.bhati@gmail.com>
2020-03-23 18:13:11 +0530

    pilot

---
+++ Drum-Kit/script.js
@@ -0,0 +1,9 @@
+function myFunction(id,url){
+    // document.getElementById(id).className = "played";
+    // myVar = setTimeout(removeclass, 2000);
+    // document.getElementById(id).className = "btnbox";
+    new Audio(url).play();
+}
+function removeclass(){
+
+}
